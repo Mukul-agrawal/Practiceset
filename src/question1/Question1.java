e03d3c67219d20ad4c93b014a65e5136e1e39d78
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2019-08-26 01:33:43 +0530

    take two input

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in)
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
     }
 }
 
