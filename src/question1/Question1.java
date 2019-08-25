dfed9380fad25b5b83b14f276875fdf8cf891583
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2019-08-26 01:34:25 +0530

    add print method to print output

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -7,6 +7,11 @@ public class Question1 {
         Scanner sc=new Scanner(System.in)
         int number1 = sc.nextInt();
         int number2 = sc.nextInt();
+        System.out.println(number1 + "+" + number2 + "=" + (number1 + number2));
+        System.out.println(number1 + "-" + number2 + "=" + (number1 - number2));
+        System.out.println(number1 + "*" + number2 + "=" + (number1 * number2));
+        System.out.println(number1 + "%" + number2 + "=" + (number1 % number2));
+        System.out.println(number1 + "/" + number2 + "=" + (number1 / number2));
     }
 }
 
