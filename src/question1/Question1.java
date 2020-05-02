1bd484847030377a43c91e2ac8af83785cf25507
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-02 15:22:41 +0530

    final commit

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 public class Question1 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in)
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two number :");
         int number1 = sc.nextInt();
         int number2 = sc.nextInt();
-        System.out.println(number1 + "+" + number2 + "=" + (number1 + number2));
-        System.out.println(number1 + "-" + number2 + "=" + (number1 - number2));
-        System.out.println(number1 + "*" + number2 + "=" + (number1 * number2));
-        System.out.println(number1 + "%" + number2 + "=" + (number1 % number2));
-        System.out.println(number1 + "/" + number2 + "=" + (number1 / number2));
+        System.out.println("Addition : " + "\n" + number1 + "+" + number2 + " = " + (number1 + number2));
+        System.out.println("Subtraction : " + "\n" + number1 + "-" + number2 + " = " + (number1 - number2));
+        System.out.println("Multiplication : " + "\n" + number1 + "*" + number2 + " = " + (number1 * number2));
+        System.out.println("Remainder : " + "\n" + number1 + "%" + number2 + " = " + (number1 % number2));
+        System.out.println("Division : " + "\n" + number1 + "/" + number2 + " = " + (number1 / number2));
     }
 }
 
